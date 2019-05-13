30f338997296bd7d99a0cc7766d6abe9cd8edb7b
powelljb <powelljb@rose-hulman.edu>
2019-05-13 08:02:35 -0400

    Done.

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,31 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Jake Powell and Sam Alvarez.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+# DONE: 2. Implement Hangman using your Iterative Enhancement Plan.
+import random
 
 ####### Do NOT attempt this assignment before class! #######
+
+## This was done in a group with Sam Alvares, Chloe Rife and I.  Sam Alvares pushed our fully implemented Hangman code.
+## Dr. Mutchler told me this was acceptable to turn in considering we did it as a group on Sam's computer.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
